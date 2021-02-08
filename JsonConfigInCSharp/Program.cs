d97bbe79523493ab178da22c19044bd01ffd2696
kienboeck <kienboec@technikum-wien.at>
2021-02-08 15:25:56 +0100

    first working version

---
+++ JsonConfigInCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace JsonConfigInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration.configurationbuilder
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("settings.json", false, true) // add as content / copy-always
+                .Build();
+
+            Console.WriteLine(config["simple"]);
+            Console.WriteLine(config["complex:but_very:simple"]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # JsonConfigInCSharp
+
+Nugets
+
+- Microsoft.Extensions.Configuration
+- Microsoft.Extensions.Configuration.FileExtensions
+- Microsoft.Extensions.Configuration.Json
+
+additional possibilities:
+
+- Microsoft.Extensions.Configuration.EnvironmentVariables
+- Microsoft.Extensions.Configuration.CommandLine
+- Microsoft.Extensions.Configuration.Ini
+- Microsoft.Extensions.Configuration.Xml
+- Microsoft.Extensions.Configuration.AzureKeyVault
+- ...
